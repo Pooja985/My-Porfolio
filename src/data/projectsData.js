30b600198ee602b1c614ea10bb880a4d7a430087
Pooja Makwana <131259381+Pooja985@users.noreply.github.com>
2025-06-15 16:53:30 +0530

    My Portfolio

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -55,47 +55,4 @@ export const projectsData = [
     demo: "https://url-shortner-wbhf.onrender.com",
     image: five,
   },
-  //   {
-  //     id: 6,
-  //     projectName: "URL Shotner",
-  //     projectDesc: "A simple and efficient web app that shortens long URLs into compact, shareable links.",
-  //     tags: ["Node.js", "Express", "MongoDB", "EJS"],
-  //     code: "https://github.com/Pooja985/URL-Shortner",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: six,
-  //   },
-  //   {
-  //     id: 7,
-  //     projectName: "Stock Market App",
-  //     projectDesc: "A simple stock market API app",
-  //     tags: ["React", "Redux", "Bootstrap"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: seven,
-  //   },
-  //   {
-  //     id: 8,
-  //     projectName: "Car Pooling System",
-  //     projectDesc:
-  //       "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
-  //     tags: ["Flutter", "React"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: eight,
-  //   },
 ];
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/
